9b1dba2c4eb276bd21588ec7632dd49217c4ff60
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-03-24 17:01:24 -0600

    Test and initial implementation for KeyGenerator#get_rotation

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,9 +1,22 @@
 class KeyGenerator
+  attr_reader :key
 
   def generate_new_key
-    key = ""
-    key << rand(9).to_s until key.length == 5
+    @key = ""
+    @key << rand(9).to_s until key.length == 5
     key
+  end
+
+  def get_rotation(rotation)
+    if rotation == "a"
+      key[0..1]
+    elsif rotation == "b"
+      key[1..2]
+    elsif rotation == "c"
+      key[2..3]
+    else 
+      key[3..4]
+    end
   end
   
 end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -8,9 +8,15 @@ class TestKeyGenerator < Minitest::Test
     assert_equal true, key_generator = !nil
   end
 
-  def test_generate_new_key_returns_number
+  def test_generate_new_key_return_value
     key_generator = KeyGenerator.new
     assert_equal String, key_generator.generate_new_key.class
     assert_equal 5, key_generator.generate_new_key.length
+  end
+
+  def test_get_rotation_return_value
+    key_generator = KeyGenerator.new
+    key_generator.generate_new_key
+    assert_equal 2, key_generator.get_rotation("a").length
   end
 end
