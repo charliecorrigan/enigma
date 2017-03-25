244e8356efc912d9a17f1354ebbaaaf90bca6e75
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-03-25 12:18:45 -0600

    Test and implement access to KeyGenerator#generate_keys by EncryptionGenerator

--- lib/encryption_generator.rb
+++ lib/encryption_generator.rb
@@ -1,3 +1,16 @@
+require './lib/character_map'
+require './lib/key_generator'
+
 class EncryptionGenerator
-character_map = CharacterMap.new
+  attr_reader :character_map, :rotation_a,  :rotation_b,  :rotation_c,  :rotation_d
+
+  def initialize
+    @character_map = CharacterMap.new
+    key_generator = KeyGenerator.new
+    key_generator.generate_new_key
+    @rotation_a = key_generator.get_rotation("a")
+    @rotation_b = key_generator.get_rotation("b")
+    @rotation_c = key_generator.get_rotation("c")
+    @rotation_d = key_generator.get_rotation("d")
+  end
 end
--- test/encryption_generator_test.rb
+++ test/encryption_generator_test.rb
@@ -7,4 +7,14 @@ class TestEncryptionGenerator < Minitest::Test
     encryption_generator = EncryptionGenerator.new
     refute_nil encryption_generator
   end
+
+  def test_access_to_character_map
+    encryption_generator = EncryptionGenerator.new
+    assert_equal 1, encryption_generator.character_map.map["a"]
+  end
+
+  def test_access_to_generate_keys
+    encryption_generator = EncryptionGenerator.new
+    assert_equal String, encryption_generator.rotation_a.class
+  end
 end
