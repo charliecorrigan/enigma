a8e4fb5e551366e1acd57c8be3000090a37d502f
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-03-25 10:27:48 -0600

    Add a test for existence of OffsetGenerator class

--- test/offset_generator_test.rb
+++ test/offset_generator_test.rb
@@ -2,7 +2,13 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/offset_generator'
 
-class TestOffseteGenerator < Minitest::Test
+class TestOffsetGenerator < Minitest::Test
+
+  def test_existence_of_offset_generator_class
+    offset_generator = OffsetGenerator.new
+    refute_nil offset_generator
+  end
+
 #TODO figure out a way to update date in test automatically
   def test_generate_date_return_value
     offset_generator = OffsetGenerator.new
