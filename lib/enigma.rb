f4fd5f2c136a109584c1b2a63e27ed47abbdbd21
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-03-27 14:13:10 -0600

    Implement Enigma#encrypt and Enigma#decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,13 @@
 require './lib/encryption_generator'
 
 class Enigma
-  def encrypt(message, key, date)
+  def encrypt(message, key = nil, date = nil)
     encryption_generator = EncryptionGenerator.new(message, key, date)
     encryption_generator.generate_cipher("encrypt")
+  end
+
+  def decrypt(message, key = nil, date = nil)
+    encryption_generator = EncryptionGenerator.new(message, key, date)
+    encryption_generator.generate_cipher("decrypt")  
   end
 end
